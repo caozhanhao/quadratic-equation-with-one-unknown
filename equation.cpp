2557f5fb9e2c9c2b9e958fa1d498e1eac9edddcb
caozhanhao <64004730+caozhanhao@users.noreply.github.com>
2020-12-25 19:16:31 -0600

    Update equation.cpp

--- equation.cpp
+++ equation.cpp
@@ -219,7 +219,7 @@ int main()
     {
         cout << "输入a,b,c:" ;
         cin >> a >> b >> c;
-        if(a == 0 & b == 0 && c == 0)
+        if(a == 0 && b == 0 && c == 0)
         {
             cout << "请输入正确的值。" << endl;
             break;
